0a880d3461ed1c79e8deda8c10e7ff07f84ecded
Nirob360e <mdminarulislam9400@gmail.comm>
2021-10-10 19:15:33 +0600

    code update

--- js/app.js
+++ js/app.js
@@ -1,2 +1,19 @@
-const inputFocus = document.querySelector('#type-name');
-inputFocus.focus();
+function focusCampo(){
+    var inputField = document.getElementById('type_name');
+    if (inputField != null && inputField.value.length != 0){
+        if (inputField.createTextRange){
+            var FieldRange = inputField.createTextRange();
+            FieldRange.moveStart('character',inputField.value.length);
+            FieldRange.collapse();
+            FieldRange.select();
+        }else if (inputField.selectionStart || inputField.selectionStart == '0') {
+            var elemLen = inputField.value.length;
+            inputField.selectionStart = elemLen;
+            inputField.selectionEnd = elemLen;
+            inputField.focus();
+        }
+    }else{
+        inputField.focus();
+    }
+}
+focusCampo();
