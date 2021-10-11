7909b0a808fc48baab30bfa1f7309480d366c51f
Nirob360e <mdminarulislam9400@gmail.comm>
2021-10-11 09:01:20 +0600

    update file code

--- js/app.js
+++ js/app.js
@@ -1,13 +1,33 @@
+const tab_btn = document.querySelectorAll(".tab-button");
+const contant = document.querySelectorAll('.content');
+
+tab_btn.forEach((tab,index)=>{
+   tab.addEventListener("click",()=>{
+    // to remove active class from previus 
+    tab_btn.forEach((tab)=> tab.classList.remove("active"));
+       // add active class
+    tab.classList.add("active");
+    // show content according to tab selected 
+    contant.forEach((c)=> c.classList.remove("active") );
+
+    // active content 
+    contant[index].classList.add("active");
+   })
+
+})
+
+// curser focus code 
+
 function focusCampo(){
-    var inputField = document.getElementById('type_name');
+    let inputField = document.getElementById('type_name');
     if (inputField != null && inputField.value.length != 0){
         if (inputField.createTextRange){
-            var FieldRange = inputField.createTextRange();
+            let FieldRange = inputField.createTextRange();
             FieldRange.moveStart('character',inputField.value.length);
             FieldRange.collapse();
             FieldRange.select();
         }else if (inputField.selectionStart || inputField.selectionStart == '0') {
-            var elemLen = inputField.value.length;
+            let elemLen = inputField.value.length;
             inputField.selectionStart = elemLen;
             inputField.selectionEnd = elemLen;
             inputField.focus();
@@ -16,4 +36,11 @@ function focusCampo(){
         inputField.focus();
     }
 }
+// active input focus
 focusCampo();
+
+
+
+
+
+
